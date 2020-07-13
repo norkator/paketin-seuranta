51b8941cb113a0be32a141b18437d61f84ddf0c8
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2020-07-13 17:47:54 +0300

    DHLExpress okHttp

--- app/src/main/java/com/nitramite/courier/DHLExpressStrategy.java
+++ app/src/main/java/com/nitramite/courier/DHLExpressStrategy.java
@@ -2,83 +2,50 @@ package com.nitramite.courier;
 
 import android.annotation.SuppressLint;
 import android.util.Log;
+
 import com.nitramite.paketinseuranta.EventObject;
 import com.nitramite.utils.Utils;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
-public class DHLExpressStrategy implements CourierStrategy, HostnameVerifier {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DHLExpressStrategy implements CourierStrategy {
 
     // Logging
     private static final String TAG = "DHLExpressStrategy";
-
-
-    // Host name verifier
-    public boolean verify(String hostname, SSLSession session) {
-        return true;
-    }
 
 
     @Override
     public ParcelObject execute(String parcelCode) {
         ParcelObject parcelObject = new ParcelObject(parcelCode);
         ArrayList<EventObject> eventObjects = new ArrayList<>();
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-                    @SuppressLint("TrustAllX509TrustManager")
-                    public void checkClientTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                    @SuppressLint("TrustAllX509TrustManager")
-                    public void checkServerTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
         try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
             String url = "https://www.dhl.fi/shipmentTracking?AWB=" + parcelCode + "&countryCode=fi&languageCode=fi";
-            URL obj = new URL(url);
-            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-            con.setConnectTimeout(5000);    // Timeout for connecting
-            con.setReadTimeout(5000);       // Timeout for reading content
-            con.setSSLSocketFactory(sc.getSocketFactory());
-            con.setHostnameVerifier(this);
-            con.setRequestMethod("GET");
-            String USER_AGENT = "Mozilla/5.0";
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            String jsonResult = response.toString();
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .addHeader("User-Agent", "Mozilla/5.0")
+                    .addHeader("Host", "www.dhl.fi")
+                    .addHeader("Content-Type", "application/json;charset=UTF-8")
+                    .addHeader("Connection", "keep-alive")
+                    .addHeader("Cache-Control", "max-age=0, no-cache, no-store")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String jsonResult = response.body().string();
+
 
             // Parsing got json content
             JSONObject jsonResponse = new JSONObject(jsonResult);                       // Json content
@@ -164,11 +131,7 @@ public class DHLExpressStrategy implements CourierStrategy, HostnameVerifier {
         } catch (JSONException e) {
             e.printStackTrace();
             Log.i(TAG, e.toString());
-        } catch (IOException | KeyManagementException | NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e) {
+        } catch (IOException | ParseException | NullPointerException e) {
             e.printStackTrace();
         }
         return parcelObject;
--- app/src/main/java/com/nitramite/courier/PostiStrategy.java
+++ app/src/main/java/com/nitramite/courier/PostiStrategy.java
@@ -16,7 +16,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Objects;
 
 import javax.net.ssl.SSLException;
 
