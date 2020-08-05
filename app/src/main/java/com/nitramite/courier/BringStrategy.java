eb84bf3859d99712c650aa14d200bb7cafc08076
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2020-08-05 18:42:31 +0300

    Update BringStrategy.java

--- app/src/main/java/com/nitramite/courier/BringStrategy.java
+++ app/src/main/java/com/nitramite/courier/BringStrategy.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.util.Log;
 
 import com.nitramite.paketinseuranta.EventObject;
-import com.nitramite.utils.Utils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,13 +32,10 @@ public class BringStrategy implements CourierStrategy {
         String url = "https://tracking.bring.com/tracking/api/fetch/" + parcelCode + "?lang=en";
 
         ParcelObject parcelObject = new ParcelObject(parcelCode);
-        ArrayList<EventObject> eventObjects = new ArrayList<>();
         try {
             OkHttpClient client = new OkHttpClient();
-
             Request request = new Request.Builder()
                     .url(url)
-                    .addHeader("Host", "tracking.bring.com")
                     .addHeader("User-Agent", Constants.UserAgent)
                     .build();
             Response response = client.newCall(request).execute();
@@ -53,53 +49,11 @@ public class BringStrategy implements CourierStrategy {
 
 
             if (hasParcelDetails(consignmentSet, "packageSet")) { // parcel details exists
-                parcelObject.setIsFound(true);
-
                 JSONObject packageSet = consignmentSet.getJSONArray("packageSet").getJSONObject(0);
-                parcelObject.setWeight(packageSet.optString("weightInKgs"));
-                parcelObject.setHeight(packageSet.optString("heightInCm"));
-                parcelObject.setWidth(packageSet.optString("widthInCm"));
-                parcelObject.setDepth(packageSet.optString("lengthInCm"));
-                parcelObject.setVolume(packageSet.optString("volumeInDm3"));
-                parcelObject.setSender(packageSet.optString("senderName"));
 
-                // Get events
-                JSONArray jsonEvents = packageSet.getJSONArray("eventSet");
-
-                // Only events have "phase" status, first is newest
-                parcelObject.setPhase(jsonEvents.optJSONObject(0).optString("status"));
-
-                @SuppressLint("SimpleDateFormat") DateFormat apiDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-                @SuppressLint("SimpleDateFormat") DateFormat showingDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-                @SuppressLint("SimpleDateFormat") DateFormat SQLiteDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                for (int i = 0; i < jsonEvents.length(); i++) {
-                    JSONObject event = jsonEvents.getJSONObject(i);
-
-
-                    String timeStamp = event.optString("dateIso");
-                    Date parseTimeDate = Utils.postiOffsetDateHours(apiDateFormat.parse(timeStamp));
-                    final String parsedDate = showingDateFormat.format(parseTimeDate);
-                    final String parsedDateSQLiteFormat = SQLiteDateFormat.format(parseTimeDate);
-
-
-                    // Get event description
-                    String eventDescription = event.optString("description");
-                    String locationCode = event.optString("postalCode");
-                    String locationName = event.optString("city") + ", " +
-                            event.optString("country");
-
-                    EventObject eventObject = new EventObject(
-                            eventDescription,
-                            parsedDate,
-                            parsedDateSQLiteFormat,
-                            locationCode,
-                            locationName
-                    );
-                    // Add object
-                    eventObjects.add(eventObject);
-                }
-                parcelObject.setEventObjects(eventObjects); // Set event object into parcel object for later fetching
-
+                JSONArray jsonEvents = packageSet.getJSONArray("eventSet"); // Get events
+                setParcelDetails(parcelObject, packageSet, jsonEvents);
+                parcelObject.setEventObjects(setParcelEvents(jsonEvents));
 
             } else {
                 Log.i(TAG, "Bring shipment not found");
@@ -111,16 +65,91 @@ public class BringStrategy implements CourierStrategy {
         } catch (IOException e) {
             Log.i(TAG, e.toString());
             e.printStackTrace();
-        } catch (NullPointerException | ParseException e) {
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
         return parcelObject;
     }
 
 
+    /**
+     * Checks key existence from json object
+     *
+     * @param jsonObject any json object
+     * @param keyName    key to look for
+     * @return boolean
+     */
     private boolean hasParcelDetails(JSONObject jsonObject, String keyName) {
         return jsonObject.has(keyName);
     }
 
 
-} // End of class
+    /**
+     * Set's basic parcel details
+     *
+     * @param parcelObject json object of parcel where data is saved
+     * @param packageSet   parcel basic data object
+     * @param jsonEvents   parcel events array
+     */
+    private void setParcelDetails(ParcelObject parcelObject, JSONObject packageSet, JSONArray jsonEvents) {
+        parcelObject.setIsFound(true);
+        parcelObject.setPhase(jsonEvents.optJSONObject(0).optString("status")); // Only events have "phase" status, first is newest
+        parcelObject.setWeight(packageSet.optString("weightInKgs"));
+        parcelObject.setHeight(packageSet.optString("heightInCm"));
+        parcelObject.setWidth(packageSet.optString("widthInCm"));
+        parcelObject.setDepth(packageSet.optString("lengthInCm"));
+        parcelObject.setVolume(packageSet.optString("volumeInDm3"));
+        parcelObject.setSender(packageSet.optString("senderName"));
+    }
+
+
+    /**
+     * Parse and add parcel events to event objects
+     *
+     * @param jsonEvents events for parcel
+     * @return event objects array
+     */
+    private ArrayList<EventObject> setParcelEvents(JSONArray jsonEvents) {
+        ArrayList<EventObject> eventObjects = new ArrayList<>();
+        try {
+            @SuppressLint("SimpleDateFormat") DateFormat apiDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            @SuppressLint("SimpleDateFormat") DateFormat showingDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            @SuppressLint("SimpleDateFormat") DateFormat SQLiteDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            for (int i = 0; i < jsonEvents.length(); i++) {
+                JSONObject event = jsonEvents.getJSONObject(i);
+
+
+                String timeStamp = event.optString("dateIso");
+                Date parseTimeDate = null;
+
+                parseTimeDate = apiDateFormat.parse(timeStamp);
+
+                final String parsedDate = showingDateFormat.format(parseTimeDate);
+                final String parsedDateSQLiteFormat = SQLiteDateFormat.format(parseTimeDate);
+
+
+                // Get event description
+                String eventDescription = event.optString("description");
+                String locationCode = event.optString("postalCode");
+                String locationName = event.optString("city") + ", " +
+                        event.optString("country");
+
+                EventObject eventObject = new EventObject(
+                        eventDescription,
+                        parsedDate,
+                        parsedDateSQLiteFormat,
+                        locationCode,
+                        locationName
+                );
+                // Add object
+                eventObjects.add(eventObject);
+
+            }
+        } catch (ParseException | JSONException e) {
+            e.printStackTrace();
+        }
+        return eventObjects;
+    }
+
+
+}
