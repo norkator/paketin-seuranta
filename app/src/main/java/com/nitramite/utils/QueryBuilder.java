048190a5b016da2400b3cdaa591dbef2d3840775
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-04-16 14:18:10 +0300

    Skeleton for query builder

---
+++ app/src/main/java/com/nitramite/utils/QueryBuilder.java
@@ -0,0 +1,24 @@
+package com.nitramite.utils;
+
+public class QueryBuilder {
+
+    public enum Query {
+        SELECT,
+        FROM,
+        JOIN,
+        WHERE,
+        ORDER_BY,
+    }
+
+
+    public QueryBuilder() {
+
+    }
+
+    
+
+    public String Build() {
+        return null;
+    }
+
+}
