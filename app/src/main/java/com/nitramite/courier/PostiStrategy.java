c9d04eb5bfe497cc7ae58998035291fceb0f57de
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-04-04 20:47:26 +0300

    add graphql call required headers

--- app/src/main/java/com/nitramite/courier/PostiStrategy.java
+++ app/src/main/java/com/nitramite/courier/PostiStrategy.java
@@ -32,86 +32,22 @@ public class PostiStrategy implements CourierStrategy {
 
 
     private static final String TOKEN_URL = "https://auth-service.posti.fi/api/v1/anonymous_token";
+    private static final String PARCEL_URL = "https://oma.posti.fi/graphql/v2";
 
 
     @Override
     public ParcelObject execute(final String parcelCode, final Locale locale) {
         ParcelObject parcelObject = new ParcelObject(parcelCode);
         ArrayList<EventObject> eventObjects = new ArrayList<>();
-
         try {
             PostiToken postiToken = getTokens();
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.i(TAG, e.toString());
-        } catch (SSLException e) {
-            Log.i(TAG, "RESET BY PEER FOR " + parcelCode);
-            parcelObject.setUpdateFailed(true);
-            e.printStackTrace();
-        } catch (IOException | ParseException | NullPointerException e) {
-            e.printStackTrace();
-        }
-
-        return parcelObject;
-    }
-
-
-    /**
-     * Get required tokens for graphql call
-     *
-     * @return
-     * @throws IOException
-     * @throws JSONException
-     */
-    private PostiToken getTokens() throws IOException, JSONException {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(TOKEN_URL)
-                .addHeader("User-Agent", Constants.UserAgent)
-                .addHeader("content-type", Constants.ContentType)
-                .build();
-        Response response = client.newCall(request).execute();
-        String body = response.body().string();
-        JSONObject jsonBody = new JSONObject(body);
-        JSONArray roleTokens = jsonBody.optJSONArray("role_tokens");
-        JSONObject first = roleTokens.optJSONObject(0);
-        return new PostiToken(
-                jsonBody.optString("id_token"),
-                first.optString("token")
-        );
-    }
-
-
-    /**
-     * Object holding tokens
-     */
-    private class PostiToken {
-        private String idToken;
-        private String roleToken;
-
-        PostiToken(String idToken, String roleToken) {
-            this.idToken = idToken;
-            this.roleToken = roleToken;
-        }
-
-        public String getIdToken() {
-            return idToken;
-        }
-
-        public String getRoleToken() {
-            return roleToken;
-        }
-
-    }
-
-    /*
-       try {
-            String url = "https://www.posti.fi/henkiloasiakkaat/seuranta/api/shipments/" + parcelCode;
-
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
-                    .url(url)
+                    .url(PARCEL_URL)
                     .addHeader("User-Agent", Constants.UserAgent)
+                    .addHeader("content-type", Constants.ContentType)
+                    .addHeader("authorization", "Bearer " + postiToken.getIdToken())
+                    .addHeader("x-omaposti-roles", postiToken.getRoleToken())
                     .build();
             Response response = client.newCall(request).execute();
             String jsonResult = response.body().string();
@@ -177,7 +113,7 @@ public class PostiStrategy implements CourierStrategy {
                 if (jsonChildNode.getJSONObject("product").has("name")) {
                     if (!jsonChildNode.getJSONObject("product").has("name")) {
                         parcelObject.setProduct(
-                                jsonChildNode.getJSONObject("product").getJSONObject("name").optString(locale == Locale.FI? "fi" : "en")
+                                jsonChildNode.getJSONObject("product").getJSONObject("name").optString(locale == Locale.FI ? "fi" : "en")
                         );
                     }
                 }
@@ -190,7 +126,7 @@ public class PostiStrategy implements CourierStrategy {
                         JSONObject extraServiceNameObj = extraServiceObj.optJSONObject("name");
                         if (extraServiceNameObj != null) {
                             parcelObject.setExtraServices(
-                                    extraServiceNameObj.optString(locale == Locale.FI? "fi" : "en")
+                                    extraServiceNameObj.optString(locale == Locale.FI ? "fi" : "en")
                             );
                         }
                     }
@@ -229,7 +165,7 @@ public class PostiStrategy implements CourierStrategy {
                     JSONObject eventObj = eventJsonObj.optJSONObject("description");
                     String eventDescription = "-";
                     if (eventObj != null) {
-                        eventDescription = eventObj.optString(locale == Locale.FI? "fi" : "en");
+                        eventDescription = eventObj.optString(locale == Locale.FI ? "fi" : "en");
                     }
 
                     // Pass to object
@@ -245,6 +181,8 @@ public class PostiStrategy implements CourierStrategy {
                 Log.i(TAG, "Posti shipment not found");
                 parcelObject.setIsFound(false); // Parcel not found
             }
+
+
         } catch (JSONException e) {
             e.printStackTrace();
             Log.i(TAG, e.toString());
@@ -255,7 +193,58 @@ public class PostiStrategy implements CourierStrategy {
         } catch (IOException | ParseException | NullPointerException e) {
             e.printStackTrace();
         }
+
+        return parcelObject;
+    }
+
+
+    /**
+     * Get required tokens for graphql call
+     *
+     * @return
+     * @throws IOException
+     * @throws JSONException
      */
+    private PostiToken getTokens() throws IOException, JSONException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(TOKEN_URL)
+                .addHeader("User-Agent", Constants.UserAgent)
+                .addHeader("content-type", Constants.ContentType)
+                .build();
+        Response response = client.newCall(request).execute();
+        String body = response.body().string();
+        JSONObject jsonBody = new JSONObject(body);
+        JSONArray roleTokens = jsonBody.optJSONArray("role_tokens");
+        JSONObject first = roleTokens.optJSONObject(0);
+        return new PostiToken(
+                jsonBody.optString("id_token"),
+                first.optString("token")
+        );
+    }
+
+
+    /**
+     * Object holding tokens
+     */
+    private class PostiToken {
+        private String idToken;
+        private String roleToken;
+
+        PostiToken(String idToken, String roleToken) {
+            this.idToken = idToken;
+            this.roleToken = roleToken;
+        }
+
+        public String getIdToken() {
+            return idToken;
+        }
+
+        public String getRoleToken() {
+            return roleToken;
+        }
+
+    }
 
 
 } // End of class
